b648eed665bd1e40c425ff15274b3fe27f89efe5
Aary Jadhav <jadhavaary02@gmail.com>
2026-05-02 19:26:53 +0530

    Add checkout.js for frontend payment processing

---
+++ public/checkout.js
@@ -0,0 +1,9 @@
+// This is the checkout.js file for frontend payment processing
+
+function processPayment() {
+    // Payment processing logic goes here
+    console.log('Processing payment...');
+}
+
+// Export the processPayment function for use in other parts of the application
+export { processPayment };
